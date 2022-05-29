fd6bfaf3c62f059b9d6a4908fb3ca19759ed7f43
Aleksandar Lazarov <alleksan2@gmail.com>
2022-05-29 21:03:14 +0300

    fixed mobile

--- sections/AgencySelection/elements.jsx
+++ sections/AgencySelection/elements.jsx
@@ -21,7 +21,7 @@ export const StyledContainer = styled(({ height, ...props }) => (
   align-items: center;
 
   @media screen and (max-width: 480px) {
-    margin-left: 20%;
+    margin-left: 16%;
   }
 `;
 
