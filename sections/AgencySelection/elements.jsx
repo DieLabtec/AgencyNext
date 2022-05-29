4520d2acb1b158892a1406bef7cc8ab528898192
Aleksandar Lazarov <alleksan2@gmail.com>
2022-05-29 17:32:54 +0300

    fix

--- sections/AgencySelection/elements.jsx
+++ sections/AgencySelection/elements.jsx
@@ -75,7 +75,7 @@ export const StyledContainerContainer = styled(({ ...props }) => (
     margin-left: 27%;
   }
   @media screen and (max-width: 650px) {
-    margin-left: 25%;
+    margin-left: 15%;
   }
 `;
 
